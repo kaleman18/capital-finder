aac176839618ff486c89c74712eeb9e1c8697439
kaleman18 <kylealeman18@gmail.com>
2024-01-29 16:20:05 -0800

    test1.0

--- api/index.py
+++ api/index.py
@@ -1,11 +1,26 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import requests
  
 class handler(BaseHTTPRequestHandler):
  
     def do_GET(self):
+
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+
+        if "country" in dic:
+            response = requests.get(f"https://restcountries.com/v3.1/name/{dic['country']}?fullText=true")
+            data = response.json()
+            capital = data["capital"][0]
+            message = f"The capital of {dic['country']} is {capital}"
+
+        
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers() 
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(message.encode('utf-8'))
         return
+    
